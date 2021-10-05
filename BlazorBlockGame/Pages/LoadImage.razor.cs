f2ce8d990a8440931110396dab4fa259e4995a82
Scott Falbo <scottfalboart@gmail.com>
2021-10-04 18:00:15 -0700

    Added LoadImage component to use a custom image for game.

---
+++ BlazorBlockGame/Pages/LoadImage.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBlockGame.Pages
+{
+    public partial class LoadImage : ComponentBase
+    {
+        private void LoadFile(InputFileChangeEventArgs e)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
